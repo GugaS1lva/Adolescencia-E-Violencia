97e07a23ba4ce3ff90b1c60ebb5610b6884ec5c7
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-11-20 09:31:53 -0300

    Static-Images Version

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import img1 from './assets/1.png'
+import img2 from './assets/2.png'
+import img3 from './assets/3.png'
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-zinc-900">
+      <img src={img1} alt="" />
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
+    </div>
+  )
+}
+
+export default App
